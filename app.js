90c91a028464512e28f4b8d3187e53894e703f24
404Vector-mac <tiryul@gmail.com>
2022-04-17 14:33:01 +0900

    port error solved, add timestamp code at app.js

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+require('dotenv').config();
+/*
+require()는 module.exports를 리턴한다.
+node.js에서 외부 모듈을 가저올 때 사용.
+ */
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = process.env.PORT; // warn! mac Airplay use port 5000.
+const APP = './app/routes'
+
+console.log(`server ip is http://localhost:${port}`)
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log('***************** ***************** *****************')
+  console.log('********** 서버가 정상적으로 실행되고 있습니다 *********')
+  console.log('***************** ***************** *****************')
+})
+app.get('/', (req, res) => {
+  res.json({ "현재 시간 : ": new Date().toLocaleString() })
+})
+app.get('/api/now', cors(corsOptions), (req, res) => {
+  res.json({ "now": new Date().toLocaleString() })
 })
