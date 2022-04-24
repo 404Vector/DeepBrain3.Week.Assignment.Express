3b9289255fd8b24fe8b44fbeed339a80dad3e82e
404Vector-mac <tiryul@gmail.com>
2022-04-24 23:21:33 +0900

    change package.json add some package dp

--- server.js
+++ server.js
@@ -1,37 +1,58 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import passport from 'passport'
+import morgan from 'morgan'
+import db from './app/models/index.js'
+import api from "./app/routes/api.js"
+import basic from "./app/routes/basic.js"
+import user from "./app/routes/user.js"
+import index from "./app/routes/index.js"
+import getResponse from "./app/lambdas/getResponse.js"
+import applyPassport from './app/lambdas/applyPassport.js'
+import applyDotenv from './app/lambdas/applyDotenv.js'
 
-require('dotenv').config();
-/*
-require()는 module.exports를 리턴한다.
-node.js에서 외부 모듈을 가저올 때 사용.
- */
-const cors = require('cors');
-const express = require('express');
-const app = express();
-const port = process.env.PORT; // warn! mac Airplay use port 5000.
-const APP = './app/routes'
+async function startServer() {
+    const app = express();
+    const {mongoUri, port, jwtSecret} = applyDotenv(dotenv)
+    app.use(express.static('public'));
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    const _passport = applyPassport(passport, jwtSecret);
+    app.use(_passport.initialize());
+    app.use("/", index);
+    app.use("/api", api);
+    app.use("/basic", basic);
+    app.use("/user", user);
+    app.use(morgan('dev'))
+    db
+        .mongoose
+        .connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => {
+            console.log(' ### 몽고DB 연결 성공 ### ')
+        })
+        .catch(err => {
+            console.log(' 몽고DB와 연결 실패', err)
+            process.exit();
+        });
 
-console.log(`server ip is http://localhost:${port}`)
+    app.all("*", function (_req, res) {
+        return getResponse.notFoundResponse(res, "페이지를 찾을 수 없습니다");
+    });
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+    app.use((err, _req, res) => {
+        if (err.name == "UnauthorizedError") {
+            return getResponse.unauthorizedResponse(res, err.message);
+        }
+    });
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200
+    app.listen(port, () => {
+        console.log('***************** ***************** *****************')
+        console.log('********** 서버가 정상적으로 실행되고 있습니다 *********')
+        console.log('***************** ***************** *****************')
+    })
+
 }
-
-app.listen(port, () => {
-  console.log('***************** ***************** *****************')
-  console.log('********** 서버가 정상적으로 실행되고 있습니다 *********')
-  console.log('***************** ***************** *****************')
-})
-
-// app.get(~) get 상태만을 허용하는 api를 추가
-app.get('/', (req, res) => {
-  res.json({ "현재 시간 : ": new Date().toLocaleString() })
-})
-app.get('/api/now', cors(corsOptions), (req, res) => {
-  res.json({ "now": new Date().toLocaleString() })
-})
+startServer()
